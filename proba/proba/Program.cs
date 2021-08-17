12f26a26c99cd03a93c8056250d43b41275601b7
Mirna Ladnjak <mirna.ladnjak@gmail.com>
2021-08-17 10:42:01 +0200

    small change

--- proba/proba/Program.cs
+++ proba/proba/Program.cs
@@ -7,6 +7,7 @@ namespace proba
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World?");
         }
     }
 }
